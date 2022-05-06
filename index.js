a491cd49bcda5bcf512ac8c0437bf0dbfff57987
Daniel Juarez <juarezdaniel@gmail.com>
2022-05-06 13:24:10 -0400

    added web socket

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 
-const { WebClient } = require('@slack/web-api');
+const { WebClient } = require('@slack/web-api')
+const { SocketModeClient } = require('@slack/socket-mode');
+const { createEventAdapter } = require('@slack/events-api');
 
+const port = process.env.PORT || 3000;
 const slackToken = new WebClient(process.env.SLACK_TOKEN);
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackEvents = createEventAdapter(slackSigningSecret);
+const socketToken = process.env.SLACK_APP_TOKEN;
+const socketMode = new SocketModeClient({ socketToken });
 
 (async () => {
     try {
@@ -15,4 +21,13 @@ const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
     } catch (error) {
         console.log(error);
     }
+})();
+
+slackEvents.on('message', (event) => {
+    console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+(async () => {
+    const server = await slackEvents.start(port);
+    console.log(`Listening for events on ${server.address().port}`);
 })();
