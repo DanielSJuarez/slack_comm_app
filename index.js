4cc1b3a1e70d07094d2479a5733e6ec11d2e9563
Daniel Juarez <juarezdaniel@gmail.com>
2022-05-09 09:53:04 -0400

    help flow

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 require('dotenv').config()
-
 const { WebClient } = require('@slack/web-api')
 const { SocketModeClient } = require('@slack/socket-mode');
-const { createEventAdapter } = require('@slack/events-api');
+// const { createEventAdapter } = require('@slack/events-api');
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+// const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+// const slackEvents = createEventAdapter(slackSigningSecret);
+
+const socketToken = process.env.SOCKET_TOKEN;
+console.log({ socketToken });
+console.log(socketToken);
+const socketClient = new SocketModeClient(socketToken);
 const slackToken = new WebClient(process.env.SLACK_TOKEN);
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
-const slackEvents = createEventAdapter(slackSigningSecret);
-const socketToken = process.env.SLACK_APP_TOKEN;
-const socketMode = new SocketModeClient({ socketToken });
 
-(async () => {
+
+const messageResponse = async (message) => {
     try {
         await slackToken.chat.postMessage({
             channel: '#app_testing',
-            text: `Test to verify that you read this`,
+            text: `I have seen your message ${message}! What can I help you with`,
         });
         console.log('Message posted!');
     } catch (error) {
         console.log(error);
     }
-})();
+}
 
-slackEvents.on('message', (event) => {
-    console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+socketClient.on('message', async ({ event }) => {
+    const message = event.message;
+    console.log(event);
+    if (message.toUpdateCase() === 'HELP') {
+        messageResponse(message);
+    }
 });
 
 (async () => {
-    const server = await slackEvents.start(port);
-    console.log(`Listening for events on ${server.address().port}`);
+    await socketClient.start();
 })();
